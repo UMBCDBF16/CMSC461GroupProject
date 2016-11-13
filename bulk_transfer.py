5b5dc6a6247bc134c92c72fc3280f21abcbcd0f0
gchan1 <gchan1@umbc.edu>
2016-11-13 15:10:45 -0500

    fixing all the stuff because we finished the code anywhere stuff!

--- bulk_transfer.py
+++ bulk_transfer.py
@@ -1,3 +1,17 @@
 #name: bulk_transfer.py
 #author: Grace Chandler
 #date: 11-13-16
+#description
+
+def parse_csv(csv,tablename):
+  
+  relations = []
+  data = open(csv,'r')
+  for line in data:
+    line = line.strip.split(',')
+    relations.append(line)
+    #more things
+   return relations 
+    
+  
+  
